5f90fb020905a579bd71ff7196d48350c7241c5f
HH305- <HH305-@HH306-44>
2016-11-16 18:16:52 +0900

    PushPrint.java.modifide on Local repository

--- helloWorld/src/helloWorld/PushPrint.java
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -3,6 +3,7 @@ package helloWorld;
 public class PushPrint {
 	public static void main(String[] args){
 		System.out.println("Add Java Class File Push");
+		System.out.println("world with local");
 	}
 
 }

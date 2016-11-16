826daf17cb0cc538e6e57d2befad7ede4426b16a
SongMinjuu <alswnzx@naver.com>
2016-11-16 18:14:54 +0900

    PushPrint.java.modifide onGitHub

--- helloWorld/src/helloWorld/PushPrint.java
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -3,6 +3,8 @@ package helloWorld;
 public class PushPrint {
 	public static void main(String[] args){
 		System.out.println("Add Java Class File Push");
+		system.out.println("world with 365");
+		
 	}
 
 }
